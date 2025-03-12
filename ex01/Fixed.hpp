6a5a69dc7b185abf8761def4817083c8d2433df9
kimia <kimiaziari1997@gmail.com>
2025-03-12 17:54:53 +0100

    add ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int value;
+	static const int bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &source);
+	Fixed(const int number);
+	Fixed(const float number);
+
+	Fixed &operator = (const Fixed &source);
+
+	~Fixed();
+	
+	int getRawBits(void);
+	void SetRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+
+
+#endif
